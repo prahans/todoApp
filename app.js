608cb9ac998579e586e10c3ad95c6813dad788e5
anurag panuhar <165539474+prahans@users.noreply.github.com>
2025-10-23 19:01:48 +0545

    remove bugs

--- app.js
+++ app.js
@@ -2,45 +2,51 @@ let inputBox = document.querySelector("#input-box");
 let btn = document.querySelector("button");
 let listContainer = document.querySelector("#list-container");
 
-function addTask(){
-    if(inputBox.value == ""){
-        alert("You must enter something");
-    }else{
-        let li = document.createElement("li");
-        li.innerText = inputBox.value;
-        listContainer.append(li);
-        let span = document.createElement("span");
-        span.innerText = "\u00d7";
-        li.append(span);
-    }
+function addTask() {
+  if (inputBox.value.trim() === "") {
+    alert("You must enter something");
+    return;
+  }
 
-    inputBox.value = "";
-    saveData();
+  let li = document.createElement("li");
+  li.textContent = inputBox.value;
+  listContainer.append(li);
+
+  let span = document.createElement("span");
+  span.textContent = "\u00d7";
+  li.append(span);
+
+  inputBox.value = "";
+  saveData();
 }
 
-btn.addEventListener("click", addTask());
+btn.addEventListener("click", addTask);
+
 inputBox.addEventListener("keydown", (e) => {
-    if(e.key === "Enter"){
-        addTask();
-    }
+  if (e.key === "Enter") {
+    addTask();
+  }
 });
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName == "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    if(e.target.tagName == "SPAN"){
-        e.target.parentElement.remove();
-        saveData()
-    }
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
 });
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showData(){
-    listContainer.innerHTML = localStorage.getItem("data");
+function showData() {
+  const savedData = localStorage.getItem("data");
+  if (savedData) {
+    listContainer.innerHTML = savedData;
+  }
 }
+
 showData();
